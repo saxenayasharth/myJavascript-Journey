3db95692bfbd7dab3d211dd2693bc2e253eb6b45
saxenayasharth <95772567+saxenayasharth@users.noreply.github.com>
2023-06-25 23:49:17 +0530

    Add files via upload

---
+++ Day5/class school {.js
@@ -0,0 +1,38 @@
+/*class school {
+     name;
+     schooladdress;
+     abc;
+
+}
+
+class educationBoard {
+    registerSchool(){
+        school.name = "abc";
+    }
+}
+
+class address {
+    street;
+    city;
+    pincode;
+}
+
+const schoolobj1 = new school();
+schoolobj.address;
+
+const schoolobj2 = new school();
+schoolobj2.name*/
+
+let a = 123;
+let b = "123";
+
+if(a==b){
+    console.log("true");
+}
+if(a===b)
+{
+    console.log("===");
+}
+else{
+    console.log();
+}
