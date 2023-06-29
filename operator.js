d25f940a7425046e29f448894ffddd06ed472a23
saxenayasharth <95772567+saxenayasharth@users.noreply.github.com>
2023-06-29 23:51:19 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,81 @@
+// Assigment Operator
+
+var x = 10;
+var y = 20;
+
+
+console.log(y-=x)  //10
+console.log(x+=y)  //30
+console.log(x*=y)  //600
+console.log(x/=y)  //30
+console.log(x%=y)  //10
+
+
+//Comparision Operator
+
+//Equals
+console.log(x == y)  //false
+
+//Strict Equal
+var z = '20'
+console.log(y === z)  // false (values are same but typeof is diff)
+
+//Not Equal
+console.log(x != y)  //true
+
+console.log(x>y) //False (It always returns boolean values)
+
+
+// Arithmetic Operators:
+
+//Increment
+var a = 10;
+a++;
+console.log(a)  //11
+
+var b = 90;
+console.log(b++)  //90
+console.log(b++)  //91
+
+//Decrement
+
+var c = 33
+console.log(c--)  //33
+console.log(c--)  //32
+
+
+//Logical Operators:
+var l = 45;
+var m = 55;
+console.log(a < b && b > a)  //true
+
+
+//Ternary Operator:
+
+
+
+
+
+
+//Comma Operator:
+
+x = (2, 3);
+console.log(x);
+// output: 3
+
+
+
+//typeof Operator:
+
+console.log(typeof m)  //number
+
+
+
+
+
+
+
+
+
+
+
