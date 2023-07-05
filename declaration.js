b2ac78bb722131436980e5d3f3bbbbfae8f57636
saxenayasharth <95772567+saxenayasharth@users.noreply.github.com>
2023-07-05 23:54:04 +0530

    Add files via upload

---
+++ declaration.js
@@ -0,0 +1,20 @@
+console.log('Hello World!')
+
+// Variable declaration
+var x;
+console.log(x);  // undefined (when variable is declared but not assigned)
+
+// Assigment
+x = 20;
+console.log(x);  // 20
+
+console.log(y); //it throws an error as y is not defined (when variable is not declared)
+
+//variable hoisting:
+
+z = 5;  // Assigned 5 to z
+console.log(z);  
+var z;
+
+
+
